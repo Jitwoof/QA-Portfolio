cc000bbbac299728ea3cb800a3f9b759cc0efa3d
Jitwoof <yarosh.yuliana@gmail.com>
2025-08-06 11:19:51 +0300

    Update test_flask_webhook.py

--- telegram_bot_project/tests/test_flask_webhook.py
+++ telegram_bot_project/tests/test_flask_webhook.py
@@ -4,7 +4,7 @@ import os
 import json
 import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from flask_app import app
+from telegram_bot_project.flask_app import app
 from telegram import Update
 
 @pytest.fixture
